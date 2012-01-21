1d15f274645304816ebc9db8613385e9f3d8274f
Chintan Parikh <timtamboy63@gmail.com>
2012-01-21 20:32:15 +0800

    Cleaning up the autoloader

--- Serene/Core/Autoload.php
+++ Serene/Core/Autoload.php
@@ -1,22 +1,18 @@
 <?php
-
+/**
+ * This class is used for Autoloading all classes
+ * For this to work, the namespacing in each class MUST follow the directory structure. E.g. anything in Serene/Core has the namespace Serene/Core.
+ * 
+ * @version 1.0
+ * @author timtamboy63 (timtamboy63@gmail.com)
+ * @license Creative Commons
+ */
 namespace Serene\Core;
 
 class Autoload
 {
 	public static function autoload($class)
 	{
-		$classParts = explode('\\', $class);
-
-	
-		array_shift($classParts);
-		$filename = BASE_PATH;
-		foreach ($classParts as $classPart)
-		{
-			$filename .= '/' . $classPart;
-		}
-		$filename .= '.php';
 		include($class .'.php');
-
 	}
 }
