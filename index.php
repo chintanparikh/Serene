002827709019305cb9acb478ea311ae390a21346
Chintan Parikh <timtamboy63@gmail.com>
2012-01-22 12:23:36 +0800

    Started Exception Handling in index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php 
+use Serene\Core\Exception as SereneException;
 define("BASE_PATH", "Serene");
 require BASE_PATH . '/base.php';
 
 $application = new Serene\Application();
-$application->start();
+try
+{
+	$application->start();
+}
+catch (SereneException\FileNotFound $e)
+{
+	print "Error: {$e->getPath()} does not exist!";
+}
